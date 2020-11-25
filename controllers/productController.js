17497925cccc6770a0ddd20bb698c857129b4566
ansorudin <ahmadansorudin@gmail.com>
2020-11-25 16:36:07 +0700

    control all product

--- controllers/productController.js
+++ controllers/productController.js
@@ -1,17 +1,36 @@
+const { all } = require('../routers/productRouter')
 const query = require('./../database/mysqlAsync')
 
 const getAllProduct = async (req, res) => {
-    let getAllProductQuery = `select * from products p
+    let getAllProductQuery = `select sum(s.stock_customer) as stock_all_gudang ,group_concat(distinct(ip.url)) as url  ,p.name, c.category_name, min(vp.price) as price,  b.brands_name from products p
     join variant_product vp on vp.products_id = p.id
+    join brands b on b.id = p.brands_id
     join stock s on s.variant_product_id = vp.id
-    where s.stock_customer > 0
-    group by p.id;`
+    join category c on c.id = p.category_id
+    join image_product ip on ip.products_id = p.id
+    group by p.id
+    having stock_all_gudang > 0;`
 
     try {
         const allProduct = await query(getAllProductQuery)
+
+        let findDuplicates = arr => arr.filter((item, index) => arr.indexOf(item) != index)
+        
+        let arrCategory = allProduct.map((val,index) => {
+            return val.category_name
+        })
+        let arrBrands = allProduct.map((val, index) => {
+            return val.brands_name
+        })
+
+        let category = [...new Set(findDuplicates(arrCategory))]
+        let brands = [...new Set(findDuplicates(arrBrands))]
+        
         res.send({
             error : false,
-            allProduct
+            allProduct,
+            category,
+            brands
         })
     } catch (error) {
         res.send({
