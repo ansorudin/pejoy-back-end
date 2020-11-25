df18627e584796d9706c88c6ecefcd817e8c2732
Ryan <ryan.fandy@gmail.com>
2020-11-25 18:34:36 +0700

    Add JSON Web Token

--- Index.js
+++ Index.js
@@ -1,7 +1,9 @@
-const express = require('express')
-const cors = require('cors')
-const LoggingAPI = require('./middleware/LoggingAPI')
-const productRouter = require('./routers/productRouter')
+const express = require('express');
+const cors = require('cors');
+const LoggingAPI = require('./middleware/LoggingAPI');
+
+const productRouter = require('./routers/productRouter');
+const userProfileRouter = require('./routers/userProfileRouter');
 
 const PORT = 2000
 
@@ -14,13 +16,12 @@ app.get('/', (req, res) => {
 })
 
 app.use(LoggingAPI)
-// for get image
+// For Get Image
 app.use('/public', express.static('public'))
-// router for products
+// Router For Products
 app.use('/products', productRouter)
+// Router For User Profile
+app.use('/member', userProfileRouter)
 
-
-
-
-app.listen(PORT, () => console.log(`API Running on port ${PORT}`))
+app.listen(PORT, () => console.log(`API Running On Port ${PORT}`))
 
