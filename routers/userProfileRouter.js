b7a709ccf776f0177b4eb4f9ee6ff98248765287
Ryan <ryan.fandy@gmail.com>
2020-12-02 19:17:26 +0700

    Pushhh

--- routers/userProfileRouter.js
+++ routers/userProfileRouter.js
@@ -1,19 +1,24 @@
 const express = require('express');
 const Router = express.Router();
 const userProfileController = require('./../controllers/userProfileController');
+const jwtVerify = require('../middleware/jwt')
 
-Router.post('/profile', userProfileController.getDataUsers)
-Router.post('/shipping-address', userProfileController.getUserShippingAddress)
-Router.post('/shipping-address/add-address', userProfileController.addShippingAddress)
-Router.get('/shipping-address/get-raja-ongkir-province', userProfileController.getRajaOngkirProvince)
-Router.get('/shipping-address/get-raja-ongkir-city', userProfileController.getRajaOngkirCity)
+Router.post('/profile', jwtVerify, userProfileController.getDataUsers)
+Router.post('/transactions', jwtVerify, userProfileController.getDataTransactionsUsers)
+Router.post('/confirm-transaction', jwtVerify, userProfileController.confirmMyTransaction)
+Router.post('/shipping-address', jwtVerify, userProfileController.getUserShippingAddress)
+Router.post('/shipping-address/add-address', jwtVerify, userProfileController.addShippingAddress)
 Router.post('/shipping-address/edit-address', userProfileController.getUsersShippingAddressToEdit)
 Router.post('/shipping-address/update-address', userProfileController.updateUsersShippingAddress)
-Router.delete('/shipping-address/delete-address/:idAddress/:idUsers', userProfileController.deleteUsersShippingAddress)
+Router.delete('/shipping-address/delete-address/:token/:idAddress', userProfileController.deleteUsersShippingAddress)
+Router.get('/shipping-address/get-raja-ongkir-province', userProfileController.getRajaOngkirProvince)
+Router.get('/shipping-address/get-raja-ongkir-city', userProfileController.getRajaOngkirCity)
+Router.get('/admin-dashboard/data-statistic', userProfileController.getDataStatistic)
+Router.post('/admin-dashboard/users-transaction/get-transactions', userProfileController.getUsersTransactions)
+Router.post('/admin-dashboard/users-transaction/deliver-product', userProfileController.deliverProductsToCustomer)
 Router.get('/admin-dashboard/warehouse-inventory', userProfileController.getWarehouseInventory)
 Router.get('/admin-dashboard/flash-sale/get-products-discount', userProfileController.getDiscountProducts)
 Router.post('/admin-dashboard/flash-sale/create-flash-sale-event', userProfileController.createFlashSaleEvent)
-Router.post('/admin-dashboard/users-transaction/get-transactions', userProfileController.getUsersTransactions)
-Router.post('/admin-dashboard/users-transaction/deliver-product', userProfileController.deliverProductsToCustomer)
+// Router.post('/try-query', userProfileController.tryQuery)
 
 module.exports = Router
